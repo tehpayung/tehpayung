35c930884f67d181e8a1e52968fade08f8822b2d
tehpayung <125689832+tehpayung@users.noreply.github.com>
2023-02-21 06:14:31 +0000

    update fitur back to top

--- js/script.js
+++ js/script.js
@@ -49,3 +49,26 @@ const countdownInterval = setInterval(function () {
       " Detik ";
   }
 }, 1000);
+
+// back to top
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#0bc023 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
